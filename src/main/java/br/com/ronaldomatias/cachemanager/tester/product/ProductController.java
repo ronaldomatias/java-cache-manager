da596dd450c01328f9f1b52459afbb142a9a3e9d
ronaldomatias <ronaldomts476@gmail.com>
2024-05-13 21:56:41 -0300

    feat: antes de fazer funcionar

---
+++ src/main/java/br/com/ronaldomatias/cachemanager/tester/product/ProductController.java
@@ -0,0 +1,23 @@
+package br.com.ronaldomatias.cachemanager.tester.product;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+	private final ProductService productService;
+
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+
+	@GetMapping("/categories")
+	public List<String> getCategories() {
+		return productService.getCategories();
+	}
+
+}
---
+++ src/test/java/Testes.java
@@ -0,0 +1,11 @@
+import br.com.ronaldomatias.cachemanager.test.Teste;
+import org.junit.Test;
+
+public class Testes {
+
+	@Test
+	public void teste1() {
+		String string = Teste.getString();
+		System.out.printf(string);
+	}
+}
